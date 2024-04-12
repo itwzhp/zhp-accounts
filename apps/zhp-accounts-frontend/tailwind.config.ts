fe6476be8237167303de03ec7f0d178223c8a093
Bartłomiej Mroziński <bmrozinski7@gmail.com>
2024-04-12 23:35:23 +0200

    add code formatter, eslint config, ui components lib, zod and clsx

--- apps/zhp-accounts-frontend/tailwind.config.ts
+++ apps/zhp-accounts-frontend/tailwind.config.ts
@@ -1,20 +1,38 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        zhp: {
+          primary: '#86a315',
+          'primary-content': '#ffffff',
+          secondary: '#587d18',
+          accent: '#80539b',
+          neutral: '#cfd3d7',
+          'base-100': '#fbfbfb',
+          info: '#194093',
+          success: '#afca0b',
+          warning: '#fbcd44',
+          error: '#e94f2D',
+        },
+      },
+    ],
+  },
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
 };
 export default config;
