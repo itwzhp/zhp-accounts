84fdc51587d4f487cf5c1afa2ff6c67ad374c49c
Bartłomiej Mroziński <bmrozinski7@gmail.com>
2024-04-29 23:08:08 +0200

    Adding navigation with drawer

--- apps/zhp-accounts-frontend/app/layout.tsx
+++ apps/zhp-accounts-frontend/app/layout.tsx
@@ -16,6 +16,11 @@ export const metadata: Metadata = {
   title: 'Aktywacja | Microsoft365 w ZHP',
 };
 
+const navigationRoutes = [
+  { label: 'Aktywacja', href: '/' },
+  { label: 'Resetuj hasło', href: '/' },
+];
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -23,39 +28,87 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pl">
-      <body className={clsx(museo.className, inter.className)}>
-        <Navbar />
-        {children}
+      <body className={clsx(museo.className, inter.className, 'drawer')}>
+        <input
+          id="navigation-drawer"
+          type="checkbox"
+          className="drawer-toggle"
+        />
+
+        <div className="drawer-content flex flex-col">
+          <Navbar navigationRoutes={navigationRoutes} />
+
+          {children}
+        </div>
+
+        <div className="drawer-side">
+          <label
+            htmlFor="navigation-drawer"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu min-h-full w-80 bg-base-200 p-4">
+            {navigationRoutes.map((route) => (
+              <li key={route.label}>
+                <Link href={route.href}>{route.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </body>
     </html>
   );
 }
 
-function Navbar() {
+function Navbar({
+  navigationRoutes,
+}: {
+  navigationRoutes: { label: string; href: string }[];
+}) {
   return (
-    <div className="navbar bg-primary text-primary-content">
-      <div className="flex-1">
+    <div className="navbar w-full bg-primary text-primary-content">
+      <div className="mx-2 flex-1 px-2">
         <Link href="/" className="flex items-center gap-2">
           <Image
             src="/images/identifier-zhp-white.png"
-            height={60}
-            width={155}
+            height={45}
+            width={100}
             alt="Związek Harcerstwa Polskiego"
           />
-          <span className="text-xl font-medium">
-            aktywacja.zhp.pl
-          </span>
+          <span>aktywacja.zhp.pl</span>
         </Link>
       </div>
 
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <Link href="/">Aktywacja</Link>
-          </li>
-          <li>
-            <Link href="/">Resetuj hasło</Link>
-          </li>
+      <div className="flex-none lg:hidden">
+        <label
+          htmlFor="navigation-drawer"
+          aria-label="open sidebar"
+          className="btn btn-square btn-ghost"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
+        </label>
+      </div>
+
+      <div className="hidden flex-none lg:block">
+        <ul className="menu menu-horizontal">
+          {navigationRoutes.map((route) => (
+            <li key={route.label}>
+              <Link href={route.href}>{route.label}</Link>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
--- apps/zhp-accounts-frontend/app/page.tsx
+++ apps/zhp-accounts-frontend/app/page.tsx
@@ -2,14 +2,16 @@
 
 export default function Home() {
   return (
-    <main className="container mx-auto px-8 py-12">
-      <div className="flex flex-col items-center gap-8">
-        <div className="prose w-full max-w-4xl">
+    <main className="container mx-auto px-4">
+      <div className="flex flex-col">
+        <div className="prose max-w-none py-12">
           <h1>Microsoft 365 w ZHP</h1>
+
           <p className="font-bold">
             Związek Harcerstwa Polskiego daje możliwość bezpłatnego korzystania z pakietu
             Microsoft 365 każdemu członkowi organizacji.
           </p>
+
           <p>
             Microsoft 365 to zestaw w pełni profesjonalnych narzędzi do współpracy i komunikacji,
             dostępny w chmurze internetowej. Harcerze, którzy chcą mieć konto w zhp.net.pl,
