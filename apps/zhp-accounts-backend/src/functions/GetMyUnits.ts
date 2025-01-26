ed6ff64a41cde3ff9af9c5d7d454193dd23ece99
Karol Grodzicki <Karol.Grodzicki@speeron.com>
2025-01-26 14:39:29 +0100

    Add first empty function

---
+++ apps/zhp-accounts-backend/src/functions/GetMyUnits.ts
@@ -0,0 +1,16 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+
+export async function GetMyUnits(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    const name = request.query.get('name') || await request.text() || 'world';
+
+    return { body: `Hello, ${name}!` };
+};
+
+app.http('GetMyUnits', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    handler: GetMyUnits,
+    route: '/units'
+});
