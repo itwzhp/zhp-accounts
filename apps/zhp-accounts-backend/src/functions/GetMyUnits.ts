f9d5738701f40600e8ea5f316caa7cbfa81889f0
Karol Grodzicki <Karol.Grodzicki@speeron.com>
2025-01-26 15:28:49 +0100

    test rename function

--- apps/zhp-accounts-backend/src/functions/GetMyUnits.ts
+++ apps/zhp-accounts-backend/src/functions/GetMyUnits.ts
@@ -3,12 +3,12 @@ import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/fu
 export async function GetMyUnits(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get('name') || await request.text() || 'world';
+    const name = request.query.get('name') || 'world';
 
     return { body: `Hello, ${name}!` };
 };
 
-app.http('GetMyUnits', {
+app.http('getMyUnits', {
     methods: ['GET'],
     authLevel: 'anonymous',
     handler: GetMyUnits,
