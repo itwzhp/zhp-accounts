a9e9ffd2e0426759b439dd9b3a49034fc3547c98
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2026-01-11 13:28:34 +0100

    add test runner

--- apps/zhp-accounts-backend/vitest.config.ts
+++ apps/zhp-accounts-backend/vitest.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from "vitest/config";
+import path from "path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@/adapters": path.resolve(__dirname, "./src/adapters"),
+      "@/use-cases": path.resolve(__dirname, "./src/use-cases"),
+      "@/entities": path.resolve(__dirname, "./src/entities"),
+      "@/frameworks": path.resolve(__dirname, "./src/frameworks"),
+    },
+  },
   test: {
     environment: "node",
     coverage: {
